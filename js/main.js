c23f76108f797eb17ad86b0993882a27cf32c320
seredokhov <seredokhovp@mail.ru>
2018-04-25 14:45:53 +0400

    add

--- js/main.js
+++ js/main.js
@@ -93,20 +93,71 @@ $(function(){
     })
 })
 
-// Модальное окно 3
+
+
+// Опрос 
 
 $(function(){
-    var modal = $('.modal_window_3'),
-        input = modal.find('.modal_input'),
-        sendBtn = modal.find('.send_modal'),
-        overlay = $('.overlay'),
-        close = $('.modal_window_3 .modal_close'),
+    var link = $('.start_test'),
+        overlay =$('.overlay'),
+        test_modal = $('.test_modal'),
+        next = $('.next_question'),
+        question_block = $('.question_block'),
+        question = $('.question'),     
+        check = question.find('input'),
+        email_modal = $('.modal_window_3'),
+        input = email_modal.find('.modal_input'),
+        sendBtn = email_modal.find('.send_modal'),
+        counter = $('.counter').find('.this'),
+        count,
+        test_close = $('.close_btn'),
+        email_close = $('.modal_window_3 .modal_close');;
 
-    close.add(overlay).on('click', function(){
-        modal.fadeOut();
-        overlay.fadeOut();
+    // По нажатию на ссылку открываем модальное окно и обновляем его,
+    // если оно уже было открыто ранее
+    link.on('click', function(){
+
+        count = 0;
+        counter.text(count + 1);
+        check.removeAttr("checked");
+
+        next_question();       
+
+        test_modal.fadeIn();
+        email_modal.fadeOut();
+        question_block.fadeIn();
+        overlay.fadeIn();
+
+        return false;
     })
 
+    next.on('click', function(){
+        
+        if (count < question.length - 1 ) {
+            count++;
+            counter.text(count + 1);
+            next_question();
+        } else {
+            email_modal.fadeIn();
+            question_block.fadeOut();
+
+        }
+        next.attr('disabled', 'disabled');
+        return false;
+    })
+
+    // Активация кнопки "далее"
+    check.on('change', function(){
+        next.removeAttr("disabled")
+    })
+
+    // Следующий вопрос
+    function next_question() {        
+        question.removeClass('active');
+        question.eq(count).addClass('active');
+    }
+
+    // Валидация формы ввода, чтобы не отправить пустое поле
     input.on('input', function(){
         var value = $(this).val();
         if (value.length > 3) {
@@ -115,9 +166,16 @@ $(function(){
             sendBtn.attr('disabled', 'disabled');
         }
     })
+
+    // Закрытие модального окна по нажатию на разные элементы
+    test_close.add(overlay).add(email_close).on('click', function(){
+        test_modal.fadeOut();
+        overlay.fadeOut();        
+        return false;
+    })
 })
 
-// Список документов для скачки
+// Выпадающий Список документов для скачки
 
 $(function(){
     var link = $('#show_documents'),
@@ -129,43 +187,15 @@ $(function(){
     })
 })
 
-// Опрос 
+
+// Выпадающий Список доп опций
 
 $(function(){
-    var link = $('.start_test'),
-        overlay =$('.overlay'),
-        modal = $('.test_modal'),
-        email_modal = $('.modal_window_3'),
-        next = $('.next_question'),
-        question_block = $('.question_block'),
-        question = $('.question'),
-        check = question.find('input'),
-        counter = $('.counter').find('.this'),
-        count = 0;
+    var link = $('.more_options_link'),
+        ul = $('.more_option_list');
 
     link.on('click', function(){
-        modal.fadeIn();
-        overlay.fadeIn();
+        ul.fadeToggle(100);
         return false;
-    })
-
-    next.on('click', function(){
-        
-        if (count < question.length - 1 ) {
-            count++;
-            counter.text(count + 1);
-            question.removeClass('active');
-            question.eq(count).addClass('active');
-        } else {
-            email_modal.fadeIn();
-            modal.fadeOut();
-
-        }
-        next.attr('disabled', 'disabled');
-        return false;
-    })
-
-    check.on('change', function(){
-        next.removeAttr("disabled")
     })
 })
